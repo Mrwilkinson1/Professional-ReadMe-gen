c9d9e5c0995108337e4154b95577e67b1ea3bbea
Alex Wilkinson <mrwilkinson1@gmail.com>
2022-08-15 18:27:55 -0400

    add function

--- auto-generated-readme.md
+++ auto-generated-readme.md
@@ -1,12 +1,28 @@
-# no
+# Professional readMe
 ## Description 
-none
+its a read me generator
 
 ## Installation
 undefined
 
 ## License
-
 undefined
+
+##Installation
+posted in the readMe
+
+##Usage
+to create readMe's
+
+##contributing
+undefined
+
+##Tests
+undefined
+
+### Email: Mrwilkinson1
+
+## github: Mrwilkinson
+
 
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+
 function renderLicenseBadge(license) {
   switch (license) {
     case 'MIT':
@@ -8,8 +7,7 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+
 function renderLicenseLink(license) {
   switch (license) {
     case 'MIT':
@@ -18,14 +16,13 @@ function renderLicenseLink(license) {
   }
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+
 function renderLicenseSection(license) {
   renderLicenseBadge(license)
   renderLicenseLink(license)
 }
 
-// TODO: Create a function to generate markdown for README
+
 function generateMarkdown(data) {
   return `# ${data.Title}
 ## Description 
@@ -35,8 +32,25 @@ ${data.Description}
 ${data.Installaton}
 
 ## License
-
 ${renderLicenseSection(data.License)}
+
+##Installation
+${data.Installation}
+
+##Usage
+${data.Usage}
+
+##contributing
+${data.contributing}
+
+##Tests
+${data.test}
+
+##Email
+${data.Email}
+
+##Github
+${data.github}
 
 
 `;
